5a21d120d4562f0b78f8577e1ad1bc5d0c4e3be3
ivan0313 <ivan0313@connect.hku.hk>
2021-08-30 22:00:52 +0800

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 function App() {
   return (
     <React.Fragment>
+      <h1>Art Club</h1>
       <BrowserRouter>
         <Route path="/" exact component={Home} />
       </BrowserRouter>
